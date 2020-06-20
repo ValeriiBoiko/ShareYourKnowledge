e76268b18bee5e0dad4f996bbd2e51c3024b964e
ValeriiBoiko <valerii.boiko.1998@gmail.com>
2020-06-20 15:07:32 +0300

    make notification's appearing/hiding with animation

--- src/component/Notification/index.js
+++ src/component/Notification/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Notification.module.css';
 
 function Notification(props) {
+    const [isVisible, setVisibility] = useState(props.visible);
     let type = props.type;
 
+    useEffect(() => {
+        setVisibility(true);
+
+        setTimeout(() => {
+          setVisibility(false);
+        }, props.time - 350)
+    }, []);
+
     return (
-        <div className={styles.container + ' ' + styles[type]}>
+        <div className={`${styles.container} ${styles[type]} ${isVisible ? styles.visible : ''}`}>
             <h3 className={styles.title}>{props.title}</h3>
             <p className={styles.message}>{props.message}</p>
         </div>
