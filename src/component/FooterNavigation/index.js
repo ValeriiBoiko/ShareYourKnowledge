c3e96f440ee2ab4d0d7c674fe56695566cc67a3a
ValeriiBoiko <valerii.boiko.1998@gmail.com>
2020-06-20 15:13:53 +0300

    add key prop to cloned children, update formatting

--- src/component/FooterNavigation/index.js
+++ src/component/FooterNavigation/index.js
@@ -2,22 +2,23 @@ import React from 'react';
 import styles from './FooterNavigation.module.css';
 
 function FooterNavigation(props) {
-    const children = props.children.map(child => {
-        if (child.props.disabled) {
-            return React.cloneElement(child, {
-                ...child.props,
-                className: child.props.className + ' ' + styles.disabled
-            });
-        }
+  const children = props.children.map((child, index) => {
+    if (child.props.disabled) {
+      return React.cloneElement(child, {
+        ...child.props,
+        key: index,
+        className: child.props.className + ' ' + styles.disabled
+      });
+    }
 
-        return child;
-    })
+    return child;
+  })
 
-    return (
-        <div className={styles.wrapper + ' ' + props.className}>
-            {children}
-        </div >
-    )
+  return (
+    <div className={styles.wrapper + ' ' + props.className}>
+      {children}
+    </div >
+  )
 }
 
 export default FooterNavigation;
